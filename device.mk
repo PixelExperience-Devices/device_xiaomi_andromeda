c366ce8db0679ff47956c8b3941b49374317dc5e
Demon000 <demonsingur@gmail.com>
2019-05-14 19:32:11 +0000

    andromeda: import lights HAL from Xiaomi msm8998-common
    Change-Id: Ieee4277f15927b4522deaec9a709809cf6db6fea

--- device.mk
+++ device.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sm8150
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
