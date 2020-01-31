720e4c5da65380fa8831aecd715da8ea28aa247c
Bruno Martins <bgcngm@gmail.com>
2020-01-31 23:10:19 +0000

    andromeda: power: Scan input devices for eligible DT2W node
    It is no longer a surprise for anyone to see Xiaomi shipping the same
    device with different displays and different displays usually mean
    different touchscreen drivers.
    
    The current double tap handling in the power HAL only takes into
    account one specific node, meant to be set in the device tree.
    However, if the touchscreen was registered as a different input device
    it would cause the feature to be broken. Address the issue by iterating
    through the input devices in order to find one supported touchscreen
    input to be used.
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I2f7fb4a8b0c9cd1a16d6c2b93602d285a191f170

--- power/Power.cpp
+++ power/Power.cpp
@@ -29,6 +29,7 @@
 
 #define LOG_TAG "QTI PowerHAL"
 
+#include <dirent.h>
 #include <android/log.h>
 #include <linux/input.h>
 #include <utils/Log.h>
@@ -65,11 +66,49 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
+int open_ts_input() {
+    int fd = -1;
+    DIR *dir = opendir("/dev/input");
+
+    if (dir != NULL) {
+        struct dirent *ent;
+
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_CHR) {
+                char absolute_path[PATH_MAX] = {0};
+                char name[80] = {0};
+
+                strcpy(absolute_path, "/dev/input/");
+                strcat(absolute_path, ent->d_name);
+
+                fd = open(absolute_path, O_RDWR);
+                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
+                    if (strcmp(name, "atmel_mxt_ts") == 0 || strcmp(name, "fts_ts") == 0 ||
+                            strcmp(name, "fts") == 0 || strcmp(name, "ft5x46") == 0 ||
+                            strcmp(name, "synaptics_dsx") == 0 ||
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
+                        break;
+                }
+
+                close(fd);
+                fd = -1;
+            }
+        }
+
+        closedir(dir);
+    }
+
+    return fd;
+}
+
 void set_feature(feature_t feature, int state) {
     switch (feature) {
-#ifdef TAP_TO_WAKE_NODE
         case POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
-            int fd = open(TAP_TO_WAKE_NODE, O_RDWR);
+            int fd = open_ts_input();
+            if (fd == -1) {
+                ALOGW("DT2W won't work because no supported touchscreen input devices were found");
+                return;
+            }
             struct input_event ev;
             ev.type = EV_SYN;
             ev.code = SYN_CONFIG;
@@ -77,7 +116,6 @@ void set_feature(feature_t feature, int state) {
             write(fd, &ev, sizeof(ev));
             close(fd);
         } break;
-#endif
         default:
             break;
     }
