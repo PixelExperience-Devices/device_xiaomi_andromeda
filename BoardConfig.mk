4ab64e60a2859e4653d375ce6de4f1dd21ef6425
Nicholas Magill <nmagill1979@gmail.com>
2020-02-07 02:41:19 +0000

    andromeda: Nuke framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,6 @@ TARGET_USES_HWC2 := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Media
 TARGET_USES_ION := true
