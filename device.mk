e8d00527a954b1c4ed8aee5127e8289a7663e02a
Nicholas Magill <nmagill1979@gmail.com>
2020-02-07 08:08:37 +0000

    andromeda: Set boot animation and aapt configs

--- device.mk
+++ device.mk
@@ -7,6 +7,14 @@
 # Inherit vendor
 $(call inherit-product, vendor/xiaomi/andromeda/andromeda-vendor.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
