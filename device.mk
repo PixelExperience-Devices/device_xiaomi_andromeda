76c1836a7840d611d99d2ca52a29e957457d99ff
Nicholas Magill <nmagill1979@gmail.com>
2020-02-07 05:06:43 +0000

    andromeda: Build USB service

--- device.mk
+++ device.mk
@@ -289,6 +289,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
