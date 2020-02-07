d7c671c4709a5fbd78c76b1a40fb05811e0a4cfe
Nicholas Magill <nmagill1979@gmail.com>
2020-02-07 05:05:14 +0000

    andromeda: Build RCS service packages

--- device.mk
+++ device.mk
@@ -249,6 +249,13 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ueventd.qcom.rc \
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
