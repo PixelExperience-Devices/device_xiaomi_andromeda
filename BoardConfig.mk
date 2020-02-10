fee8dca3e7ea955f4ccb2d377b47a43575932fbf
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 03:00:48 +0000

    andromeda-user 9 PKQ1.190321.001 V10.3.23.0.PEMEUXM release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+DEVICE_PATH := device/xiaomi/andromeda
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-01-01
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/xiaomi/andromeda/BoardConfigVendor.mk
