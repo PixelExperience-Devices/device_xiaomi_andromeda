1b07868a1b110fc4e1a24ea885e66089590f1f5e
Nicholas Magill <nmagill1979@gmail.com>
2020-02-10 20:48:19 +0000

    andromeda: Clean up andromeda makefile

--- lineage_andromeda.mk
+++ lineage_andromeda.mk
@@ -15,16 +15,19 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from andromeda device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := xiaomi
-PRODUCT_DEVICE := andromeda
-PRODUCT_MANUFACTURER := xiaomi
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_andromeda
+PRODUCT_DEVICE := andromeda
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3 5G
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_VENDOR := xiaomi
-TARGET_VENDOR_PRODUCT_NAME := andromeda
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="andromeda-user 9 PKQ1.190321.001 V10.3.23.0.PEMEUXM release-keys"
+PRODUCT_MANUFACTURER := Xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/andromeda_eea/andromeda:9/PKQ1.190321.001/V10.3.23.0.PEMEUXM:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="andromeda-user 9 PKQ1.190321.001 V10.3.23.0.PEMEUXM release-keys" \
+    PRODUCT_NAME="andromeda" \
+    TARGET_DEVICE="andromeda"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
