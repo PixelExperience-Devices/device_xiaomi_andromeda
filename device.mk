4ac1aed5eede8e42f018dd5bec60eaa483ff4532
Nicholas Magill <nmagill1979@gmail.com>
2020-06-30 17:36:34 +0100

    andromeda: Build WFD libs
    Change-Id: Ib8f8277969ce367f7011740efe83458c8d4646b6

--- device.mk
+++ device.mk
@@ -489,6 +489,8 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
+    libdisplayconfig \
+    libdisplayconfig.vendor \
     libqdMetaData \
     libqdMetaData.system \
     libqdMetaData.vendor
