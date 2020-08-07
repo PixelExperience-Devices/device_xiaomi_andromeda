46a8ac972fe1105375ffe3f44b43e8914c29c3cd
Nicholas Magill <nmagill1979@gmail.com>
2020-08-07 10:06:58 +0100

    andromeda: Add Google Camera

--- device.mk
+++ device.mk
@@ -103,8 +103,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Camera
-PRODUCT_PACKAGES += \
-    Snap
+$(call inherit-product, packages/apps/GoogleCamera/config.mk)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
