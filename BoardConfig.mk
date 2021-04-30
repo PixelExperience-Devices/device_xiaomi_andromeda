2e3f0aeea5328d332d0ef465d46c4e153048aff4
Nicholas Magill <nmagill1979@gmail.com>
2021-04-30 23:57:27 +0100

    andromeda: sepolicy: Don't ignore neverallows
    Change-Id: I27582ab20c78f8a173ee17ffcd4bad91ef73ed3c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,11 +148,6 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist \
-    /vendor/bt_firmware:/bt_firmware \
-    /vendor/dsp:/dsp \
-    /vendor/firmware_mnt:/firmware
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
@@ -185,8 +180,6 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
-SELINUX_IGNORE_NEVERALLOWS := true
-
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/andromeda-sepolicy/sepolicy.mk
 
