d0d9f4cda1b8e2cf338610075db8e872bebe1332
Nicholas Magill <nmagill1979@gmail.com>
2021-05-18 17:13:17 +0100

    andromeda: Build GPS HAL from source
    Change-Id: Ie8c5e9d5e6e88bdc4721c22957f7e41b3ce7ff01

--- device.mk
+++ device.mk
@@ -171,6 +171,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
     
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
+
+PRODUCT_PACKAGES += \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libsensorndkbridge
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
