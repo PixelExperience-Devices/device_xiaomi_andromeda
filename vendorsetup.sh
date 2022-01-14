23fea081566e669c1063b7a96c6e1d60dfdfa791
Nicholas Magill <nmagill1979@gmail.com>
2022-01-14 14:51:51 +0000

    andromeda: Compile with Clang 14.0.1
    Change-Id: I348aa2d3c47197a744d30bf70bb34a73557c8fe9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-if [ ! -d "${CLANGDIR}/clang-r433403" ]
+if [ ! -d "${CLANGDIR}/clang-r437112b" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 13.0.2${NC}"
-    mkdir ${CLANGDIR}/clang-r433403
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r433403.tar.gz -P ${CLANGDIR}
-    tar -C "${CLANGDIR}"/clang-r433403/ -zxf "${CLANGDIR}"/clang-r433403.tar.gz
-    rm ${CLANGDIR}/clang-r433403.tar.gz
+    echo -e "${GREEN}Downloading clang 14.0.1${NC}"
+    mkdir ${CLANGDIR}/clang-r437112b
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r437112b.tar.gz -P ${CLANGDIR}
+    tar -C "${CLANGDIR}"/clang-r437112b/ -zxf "${CLANGDIR}"/clang-r437112b.tar.gz
+    rm ${CLANGDIR}/clang-r437112b.tar.gz
 fi
