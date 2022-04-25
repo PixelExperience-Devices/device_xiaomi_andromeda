098f21cc7bac1bf764ad9620d482e71f00af24b3
Sal Savage <salsavage@google.com>
2022-04-25 16:42:15 -0700

    andromeda: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,7 @@ include device/xiaomi/andromeda/sepolicy/sepolicy.mk
 
 # System properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 
