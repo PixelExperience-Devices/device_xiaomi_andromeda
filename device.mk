e5ffdc6515b246b31cb5830954dab09c1b8e78f7
Nicholas Magill <nmagill1979@gmail.com>
2023-05-06 22:18:23 +0100

    andromeda: Import common Xiaomi fingerprint HIDL

--- device.mk
+++ device.mk
@@ -172,6 +172,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
     
