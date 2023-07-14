405c615c1cc033ca5872fc8e4446e9ad3d7d736d
Nicholas Magill <nmagill1979@gmail.com>
2023-07-14 19:36:41 +0100

    andromeda: Compile kernel with Clang 17.0.3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-CLANGVER=clang-r487747c
+CLANGVER=clang-r498229
 if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 17.0.2${NC}"
+    echo -e "${GREEN}Downloading clang 17.0.3${NC}"
     git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
