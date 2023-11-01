52a4ab5489426913f5bf4bf36a059470461c3716
Nicholas Magill <nmagill1979@gmail.com>
2023-11-01 15:19:47 +0000

    andromeda: Uprev perf interface lib
    * Also build system variant

--- device.mk
+++ device.mk
@@ -327,7 +327,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.3 \
+    vendor.qti.hardware.perf@2.3.vendor
 
 # Protobuf
 PRODUCT_COPY_FILES += \
