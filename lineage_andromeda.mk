ae9574db488628990d9036f223e67588d443f2cd
Nicholas Magill <nmagill1979@gmail.com>
2023-12-26 16:42:38 +0000

    andromeda: Add gapps support
    * Clone https://gitlab.com/MindTheGapps/vendor_gapps to vendor/gapps

--- lineage_andromeda.mk
+++ lineage_andromeda.mk
@@ -13,6 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Gapps
+ifeq ($(WITH_GMS), true)
+$(call inherit-product,vendor/gapps/arm64/arm64-vendor.mk)
+endif
+
 # QTI Framework boost
 TARGET_SUPPORTS_FRAMEWORK_BOOST := false
 
